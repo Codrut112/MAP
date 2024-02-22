20d27dbb7c5d3a15b269c513af5650f20597f16f
Codrut Copas <118080484+Codrut112@users.noreply.github.com>
2024-02-22 19:29:26 +0200

    Delete Social_Network/sem7 directory

--- Social_Network/sem7/src/main/java/com/example/sem7/java/paging/Pageable.java
+++
@@ -1,5 +0,0 @@
-package com.example.sem7.java.paging;
-public interface Pageable {
-    int getPageNumber();
-    int getPageSize();
-}
--- Social_Network/sem7/src/main/java/com/example/sem7/java/validators/Validator.java
+++
@@ -1,6 +0,0 @@
-package com.example.sem7.java.validators;
-
-@FunctionalInterface
-public interface Validator<T> {
-    void validate(T entity) throws ValidationException;
-}
